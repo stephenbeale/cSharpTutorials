c2479573ac675270ff09418e45e848f8fa09bdb5
Steve Beale <beale.stephen@gmail.com>
2024-08-19 10:26:26 +0100

    Delegates working

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
-﻿PrintDelegate PrintToFile = (string text) => Console.WriteLine(text);
-PrintDelegate PrintConsole = (string text) => File.AppendAllText("/logs.txt", text);
+﻿using System.IO;
 
-PrintToFile("Hello World!");
+PrintDelegate PrintToConsole = (string text) => Console.WriteLine(text);
+var logsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "logs.txt");
+
+try
+{
+    
+    if (!File.Exists("logs.txt"))
+    {
+        using (FileStream fs = File.Create(logsFilePath)) { }
+    }    
+}
+catch (Exception ex)    
+{
+    Console.WriteLine(ex.Message);
+}
+
+PrintDelegate PrintToFile = (string text) => File.AppendAllText(logsFilePath, text);
+
+static void ConnectToDatabase(PrintDelegate log)
+{
+    //Insert
+    log("Inserting record into database\n");
+    //Notify
+    log("Record has been inserted into database.");
+}
+
+ConnectToDatabase(PrintToConsole);
+ConnectToDatabase(PrintToFile);
 
 delegate void PrintDelegate(string text);
 
