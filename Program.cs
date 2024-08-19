8834522e7c73fb70e018ea16f80ec7aff95be7d8
Steve Beale <beale.stephen@gmail.com>
2024-08-19 12:54:23 +0100

    Commented delegates with order of implementation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿static void Notify (NotificationDelegate notification)
+﻿/*3 - Void function
+ * - taking our generic delegate type as an argument / parameter 
+ * - passes something to our delegate type
+ * - implemented delegate function does something with argument passed
+ */
+static void Notify(NotificationDelegate notification)
 {
-    notification("User notified by:");
+    notification("User notification");
 }
 
-NotificationDelegate EmailNotification = (string notificationType) => Console.WriteLine(notificationType + " email.");
-NotificationDelegate SMSNotification = (string notificationType) => Console.WriteLine(notificationType + " SMS.");
+/*
+ * 2 - Delegate function
+ * - returning our defined delegate type
+ * - anonymous function
+ * - does something with argument
+ */
+NotificationDelegate EmailNotification = (string notificationType) => Console.WriteLine(notificationType + " is email");
+NotificationDelegate SMSNotification = (string notificationType) => Console.WriteLine(notificationType + " is SMS");
 
-Notify(EmailNotification);
+//4 - Call our void function, passing in one of our implemented delegate functions
 Notify(SMSNotification);
+Notify(EmailNotification);
 
-delegate void NotificationDelegate(string notificationType);
+//1 - Our own 'generic' delegate type with relevant argument
+delegate void NotificationDelegate(string type);
