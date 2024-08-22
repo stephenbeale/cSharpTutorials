b9b81169e979b9226e3e1fffa4407e54caa53cfa
Steve Beale <beale.stephen@gmail.com>
2024-08-22 16:47:43 +0100

    My own pipelining example, as recorded on video.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
-﻿/*3 - Void function
- * - taking our generic delegate type as an argument / parameter 
- * - passes something to our delegate type
- * - implemented delegate function does something with argument passed
- */
-static void Notify(NotificationDelegate notification)
-{
-    notification("User notification");
-}
+﻿
+using cSharpTutorials;
 
-/*
- * 2 - Delegate function
- * - returning our defined delegate type
- * - anonymous function
- * - does something with argument
- */
-NotificationDelegate EmailNotification = (string notificationType) => Console.WriteLine(notificationType + " is email");
-NotificationDelegate SMSNotification = (string notificationType) => Console.WriteLine(notificationType + " is SMS");
+{ 
+    Pipelining pl = new Pipelining();
 
-//4 - Call our void function, passing in one of our implemented delegate functions
-Notify(SMSNotification);
-Notify(EmailNotification);
+    pl.PipelineWork();
+};
 
-//1 - Our own 'generic' delegate type with relevant argument
-delegate void NotificationDelegate(string type);
+// 1 - Static class to hold your methods, usually called 'Extensions'
+
+// 2 - Make sure all methods are static
+
+// 3 - 1st parameter of all methods is preceded by 'this'
+
+// 4 - Return type matches type of 1st parameter
