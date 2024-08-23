6c70edcaeb4475340ae07d234fa5d927c9f0b3cb
Steve Beale <beale.stephen@gmail.com>
2024-08-23 16:19:08 +0100

    Average age over 18s.

--- LinqPractice.cs
+++ LinqPractice.cs
@@ -28,6 +28,19 @@ namespace cSharpTutorials
             strsWithAStart.ToList().ForEach(x => { Console.WriteLine(x); });
 
             //Given a list of people(with properties Name and Age), find the average age of all people over 18.
+            
+            //Create people
+            Person p1 = new Person("Johnny", 21);
+            Person p2 = new Person("John", 210);
+            Person p3 = new Person("Jo", 10);
+            Person p4 = new Person("Josh", 100);
+            //Add people to list
+            var people = new List<Person> { p1, p2, p3, p4 };
+
+            //Filter to over 18s AND calculate average age in one.
+            var over18s = people.Where(x => x.Age >= 18);
+            over18s.ToList().ForEach(x => { Console.WriteLine(x); });
+
             //Create two lists of integers.Write a LINQ query to find all numbers that appear in both lists.
             //Given a list of words, use LINQ to find the longest word.
             //Create a list of integers and use LINQ to square each number and return a new list.
