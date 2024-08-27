28bf4f30010545de3bababf9f506a9242659b250
Steve Beale <beale.stephen@gmail.com>
2024-08-27 09:00:29 +0100

    Sorting by price.

--- LinqPractice.cs
+++ LinqPractice.cs
@@ -16,7 +16,7 @@ namespace cSharpTutorials
         public void DoSomeLinqPractice()
         {
             Console.WriteLine("Given a list of integers, write a LINQ query to find all even numbers.");
-            var ints = Enumerable.Range(1, 100);            
+            var ints = Enumerable.Range(1, 100);
             var evens = ints.Where(x => x % 2 == 0);
             evens.ToList().ForEach(x => { Console.Write(x + ", "); });
 
@@ -66,13 +66,13 @@ namespace cSharpTutorials
             Console.WriteLine("Given a list of words, use LINQ to find the longest word.");
             var wordList = new List<string>
             {
-                "Apple", "banana", "Orange", "Grapefruit", "watermelon", "Blueberry", "strawberry", "Pineapple", "blood-orange", "well-being", "Quick", 
+                "Apple", "banana", "Orange", "Grapefruit", "watermelon", "Blueberry", "strawberry", "Pineapple", "blood-orange", "well-being", "Quick",
                 "extraordinary", "chocolate-chip", "howl", "scorn", "Blueberry", "Pineapple"
             };
 
             var longestWordSort = wordList
                 .OrderByDescending(x => x.Length)
-                .First();            
+                .First();
             Console.WriteLine($"\nLongest word is: {longestWordSort}");
 
             Console.WriteLine("Create a list of integers and use LINQ to square each number and return a new list.");
@@ -96,11 +96,46 @@ namespace cSharpTutorials
             Console.WriteLine("Word Counts");
             Console.WriteLine(string.Join("\n", wordCounts.Select(item => $"{item.Word}: {item.Count}")));
 
-            //Create a list of products(with properties Name, Price, and Category).Use LINQ to find the most expensive product in each category.
-            //Given a string, use LINQ to count the number of vowels in it.
-            //Create a list of dates.Use LINQ to find all dates that fall on a weekend.
-            //Given a list of integers, use LINQ to find the second largest number.
-            //Create a list of students(with properties Name and Grades, where Grades is a list of integers).Use LINQ to find all students with an average grade above 80.
+            //Create a list of products(with properties Name, Price, and Category). Use LINQ to find the most expensive product in each category.
+
+            List<Product> products = new List<Product>
+            {
+                new Product("C# for Dummies", 1.99, "Books"),
+                new Product("F# for Dummies", 2.99, "Books"),
+                new Product("Dove soap", 3.99, "Toiletries"),
+                new Product("HP Laptop", 1293.99, "Computers")
+            };
+
+            var productCategories = products.GroupBy(p => p.Category)
+                .Select(group => new
+                {
+                    Category = group.Key,
+                    MostExpensiveProduct = group.OrderByDescending(p => p.Price).First()
+                });
+
+            productCategories.ToList().ForEach(item =>
+            {
+                Console.WriteLine($"Category: {item.Category}, Product: {item.MostExpensiveProduct.Name}, Price: {item.MostExpensiveProduct.Price:C2}"
+            );
+
+                //Given a string, use LINQ to count the number of vowels in it.
+                //Create a list of dates.Use LINQ to find all dates that fall on a weekend.
+                //Given a list of integers, use LINQ to find the second largest number.
+                //Create a list of students(with properties Name and Grades, where Grades is a list of integers).Use LINQ to find all students with an average grade above 80.
+            });
+    }
+    }
+
+    public class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+        public Product(string name, double price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
         }
     }
 }
