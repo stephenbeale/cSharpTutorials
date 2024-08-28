b4c8c6ef809f503f4e309e6deadc40b7aba6b10a
Steve Beale <beale.stephen@gmail.com>
2024-08-28 14:15:40 +0100

    Starting on new branch

--- Program.cs
+++ Program.cs
@@ -10,12 +10,7 @@ using cSharpTutorials;
 
     lp.DoSomeLinqPractice();
 
-
-    //File path
-    string json = File.ReadAllText(@"D:\Software Engineering\Repos");
-
-    Console.WriteLine("Deserialised Data:");
-
+    
 };
 
 // 1 - Static class to hold your methods, usually called 'Extensions'
